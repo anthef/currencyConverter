7944f717221ad8527ca28f949535663ce3bc9950
anthef <anthonyef09@gmail.com>
2024-06-08 01:38:30 +0700

    finish currency

--- lib/currency_material.dart
+++ lib/currency_material.dart
@@ -1,41 +1,68 @@
 import 'package:flutter/material.dart';
 
-class CurrencyMaterial extends StatelessWidget {
+class CurrencyMaterial extends StatefulWidget {
   const CurrencyMaterial({super.key});
 
   @override
+  State<CurrencyMaterial> createState() => _CurrencyMaterialState();
+}
+
+class _CurrencyMaterialState extends State<CurrencyMaterial> {
+  double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
+
+  final border = OutlineInputBorder(
+    borderSide: const BorderSide(
+      color: Colors.white,
+      width: 2.0,
+    ),
+    borderRadius: BorderRadius.circular(5),
+  );
+
+  void convert() {
+    setState(() {
+      result = double.tryParse(textEditingController.text) != null
+          ? double.parse(textEditingController.text) * 16280.80
+          : 0;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final border = OutlineInputBorder(
-      borderSide: const BorderSide(
-        color: Colors.white,
-        width: 2.0,
-      ),
-      borderRadius: BorderRadius.circular(5),
-    );
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.blueGrey,
+        title: const Text(
+          "Currency Converter",
+          style: TextStyle(color: Colors.white, fontFamily: 'Times New Roman'),
+        ),
+      ),
       backgroundColor: Colors.blueGrey,
       body: Center(
         child: ColoredBox(
-          color: Color.fromRGBO(20, 2, 5, 0.5),
+          color: Colors.blueGrey,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                '0',
-                style: TextStyle(
+              Text(
+                result.toString(),
+                style: const TextStyle(
                   fontSize: 45,
                   fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 255, 255, 20),
+                  color: Colors.white,
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: TextField(
+                  controller: textEditingController,
                   textCapitalization: TextCapitalization.characters,
-                  style: TextStyle(color: Colors.white),
+                  style: const TextStyle(color: Colors.white),
                   decoration: InputDecoration(
                     hintText: "Please text something",
-                    hintStyle: TextStyle(color: Colors.white),
+                    hintStyle: const TextStyle(
+                        color: Colors.white, fontFamily: 'Times New Roman'),
                     prefixIcon: const Icon(
                       Icons.monetization_on_outlined,
                       color: Colors.white,
@@ -49,11 +76,21 @@ class CurrencyMaterial extends StatelessWidget {
                       const TextInputType.numberWithOptions(decimal: true),
                 ),
               ),
-              TextButton(
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                child: TextButton(
                   onPressed: () {
-                    print('button clicked');
+                    convert();
                   },
-                  child: const Text("Click me!")),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                    minimumSize: const Size(double.infinity, 50),
+                    shape: const CircleBorder(),
+                  ),
+                  child: const Icon(Icons.money_rounded),
+                ),
+              ),
             ],
           ),
         ),
@@ -61,3 +98,5 @@ class CurrencyMaterial extends StatelessWidget {
     );
   }
 }
+
+
